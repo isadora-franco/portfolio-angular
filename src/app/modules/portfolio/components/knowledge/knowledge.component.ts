363fd449870977171676891bbdfb24a1290bcc93
Isadora Franco <isadoraecfranco@gmail.com>
2026-06-25 17:19:49 -0300

    Atualizando o ts do componente Conhecimentos

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 
-// Interface
+//interface
 import { IKnowledge } from '../../interface/IKnowledge.interface';
 
 @Component({
@@ -15,22 +15,27 @@ export class KnowledgeComponent {
     {
       src: 'assets/icons/knowledge/html5.svg',
       alt: 'Ícone de conhecimento de HTML5',
+      name: 'HTML5',
     },
     {
       src: 'assets/icons/knowledge/css3.svg',
       alt: 'Ícone de conhecimento de CSS3',
+      name: 'CSS3',
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
       alt: 'Ícone de conhecimento de JavaScript',
+      name: 'JavaScript',
     },
     {
       src: 'assets/icons/knowledge/angular.svg',
       alt: 'Ícone de conhecimento de Angular',
+      name: 'Angular',
     },
     {
       src: 'assets/icons/knowledge/nodejs.svg',
       alt: 'Ícone de conhecimento de Node.js',
+      name: 'Node.js',
     },
   ]);
 }
